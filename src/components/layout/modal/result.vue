c6290ec3057c414b5ff59a409295f7ff1e5af013
halfhelix <team@halfhelix.com>
2018-11-27 12:44:36 -0500

    adjust

---
+++ src/components/layout/modal/result.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-modal
+    v-model="showModal"    
+    size="sm"
+    ok-only
+  >
+    <h1 v-if="error" :class="{'text-danger': error}">{{error_msg}}</h1>
+    <h1 v-else>Thank you<br/>for requesting the invite!</h1>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  name: 'ResultModal',
+  data () {
+    return {
+      showModal: false,
+      error: false,
+      error_msg: ''
+    }
+  },
+  created () {
+    this.$root.$on('open-result-modal', (err) => {
+      if (err) {
+        this.error_msg = err === 'Duplicate resource or resource already exists' ? 'Your email is already in our invite list' : err
+        this.error = true
+      }
+      this.showModal = true
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  h1 {
+    font-size: 24px;    
+  }
+</style>
